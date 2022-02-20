152942e55b412e6d0e1032cf976be3831032e9b5
Emmanuel Kolade <96340524+EmmaCodez@users.noreply.github.com>
2022-02-21 07:13:23 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var menu = document.getElementById("menu");
+var headerNav = document.getElementById("headerNav");
+
+menu.addEventListener("click", function () {
+    headerNav.classList.toggle("showNav");});
+    headerNav.addEventListener("click", function () {
+        headerNav.classList.remove("showNav");
+    })
